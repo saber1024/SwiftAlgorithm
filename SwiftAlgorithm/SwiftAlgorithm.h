e1576da42c1e02565d8044f9a99927e47b5cabbf
朱帅 <charlie.zhu@tradego8.com>
2022-09-04 01:40:45 +0800

    Initial Commit

---
+++ SwiftAlgorithm/SwiftAlgorithm.h
@@ -0,0 +1,18 @@
+//
+//  SwiftAlgorithm.h
+//  SwiftAlgorithm
+//
+//  Created by zhu shuai on 2022/9/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAlgorithm.
+FOUNDATION_EXPORT double SwiftAlgorithmVersionNumber;
+
+//! Project version string for SwiftAlgorithm.
+FOUNDATION_EXPORT const unsigned char SwiftAlgorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAlgorithm/PublicHeader.h>
+
+
